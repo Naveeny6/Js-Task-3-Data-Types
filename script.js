af98455c1379df15dff2efa2f5995a8e914fd073
Naveeny6 <139541811+Naveeny6@users.noreply.github.com>
2024-10-24 11:30:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+
+// Data Types:-
+
+// Number:-
+
+console.log("Data Type: Number")
+var number = 317
+var number2 = 37.97
+console.log("number Without Decimal:" ,number)
+console.log("number With Decimal:", number2)
+console.log(typeof number)
+console.log(typeof number2)
+
+
+// String
+console.log("\nData Type: String")
+var fullname = "Naveen Yeturu"
+console.log(fullname)
+console.log(typeof fullname)
+
+// Boolean
+console.log("\nData Type: Boolean")
+
+var a = 10;
+var b = 20;
+console.log("Boolean Value:", a == b)
+
+var c = 10;
+var d = 10;
+console.log("Boolean Value:", c == d)
+
+// Arrays
+
+console.log("\nData Type: Arrays")
+
+const array = ["Naveen","Johnny","Omesh"]
+console.log(array)
+console.log(typeof array)
+
+// Object
+
+console.log("\nData Type: Object")
+
+var obj = {
+    firstName:"Naveen",
+    lastName :"Yeturu",
+    city : "Hyderabad"
+}
+console.log(obj)
+console.log("FirstName:", obj.firstName)
+console.log(typeof obj)
+
+// Undefined
+
+// Avariable without a value , has the value undefined. Type is also undefined
+
+console.log("\nData Type: undefined")
+var x;
+console.log(x)
+console.log(typeof x) 
+ 
+
+// Null
+console.log("\nData Type: null")
+var n = null
+console.log(n)
+console.log(typeof n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
